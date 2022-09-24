f6c8ccfa1d556a5894271dba671011e971477373
Wei Jie Phua <weijie_luvbb@hotmail.com>
2022-09-25 00:52:18 +0800

    change app test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          EChanged
+          updates on feature
         </p>
         <a
           className="App-link"
